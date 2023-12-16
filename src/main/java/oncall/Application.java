13accc624f6988b1ed9bfa39ee28971124d29ce9
LEE JIEUN <jieun3595@soongsil.ac.kr>
2023-12-16 14:52:09 +0900

    [FEAT] Application.java에 run 코드 입력
    .

--- src/main/java/oncall/Application.java
+++ src/main/java/oncall/Application.java
@@ -1,8 +1,12 @@
 package oncall;
 
+import oncall.controller.OncallController;
+import oncall.utils.OncallUtils;
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
-        System.out.println("test");
+        OncallController oncallController = new OncallController();
+        oncallController.run();
     }
 }
