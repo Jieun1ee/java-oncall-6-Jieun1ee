ee088ca9fd61d57640b43ecc3e66bbaabd00d914
LEE JIEUN <jieun3595@soongsil.ac.kr>
2023-12-16 16:25:52 +0900

    [FEAT] 휴일인 경우, (휴일) 출력 기능 구현
    .

--- src/main/java/oncall/view/OutputView.java
+++ src/main/java/oncall/view/OutputView.java
@@ -3,12 +3,18 @@ package oncall.view;
 import java.util.List;
 
 public class OutputView {
-    public static void printResult(int month, int days, List<String> daysName, List<String> finalWorker) {
+    public static void printResult(int month, int days, List<String> daysName, List<String> finalWorker,
+                                   List<String> specialHoliday) {
         for (int i = 0; i < days; i++) {
             int day = i + 1;
-            System.out.println(month + "월 " + day + "일 " + daysName.get(i) + " "+ finalWorker.get(i));
+            if (specialHoliday.contains(String.valueOf(day))) {
+                System.out.println(month + "월 " + day + "일 " + daysName.get(i) + "(휴일) " + finalWorker.get(i));
+                continue;
+            }
+            System.out.println(month + "월 " + day + "일 " + daysName.get(i) + " " + finalWorker.get(i));
         }
     }
+
 
     public static void printException(Exception e) {
         System.out.println(e.getMessage());
