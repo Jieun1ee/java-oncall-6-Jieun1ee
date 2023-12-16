380b18b9d9a72fcb7b455e865c617c9ccb6ef904
LEE JIEUN <jieun3595@soongsil.ac.kr>
2023-12-16 15:44:18 +0900

    [FEAT] printResult 함수 구현

--- src/main/java/oncall/view/OutputView.java
+++ src/main/java/oncall/view/OutputView.java
@@ -1,6 +1,14 @@
 package oncall.view;
 
+import java.util.List;
+
 public class OutputView {
+    public static void printResult(int month, int days, List<String> daysName, List<String> finalWorker) {
+        for (int i = 0; i < days; i++) {
+            int day = i + 1;
+            System.out.println(month + "월 " + day + "일 " + daysName.get(i) + " "+ finalWorker.get(i));
+        }
+    }
 
     public static void printException(Exception e) {
         System.out.println(e.getMessage());
