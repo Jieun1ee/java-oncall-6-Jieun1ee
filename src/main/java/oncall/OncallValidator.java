a391d04af9438ea9b00a2602c83bf79b1cc6c3e5
LEE JIEUN <jieun3595@soongsil.ac.kr>
2023-12-16 14:03:49 +0900

    [FEAT] 월 입력값을 validate하는 기능 구현
    .

--- src/main/java/oncall/OncallValidator.java
+++ src/main/java/oncall/OncallValidator.java
@@ -1,5 +1,6 @@
 package oncall;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
@@ -20,5 +21,29 @@ public class OncallValidator {
         if (input.contains(COMMA + COMMA) || input.contains(COMMA + SPACE) || input.contains(SPACE + COMMA)) {
             throw new IllegalArgumentException(ERROR_MESSAGE);
         }
+    }
+
+    public static Integer checkMonth(int num) {
+        if (num < 1 || num > 12) {
+            throw new IllegalArgumentException(ERROR_MESSAGE);
+        }
+        return num;
+    }
+
+    public static String checkStartDay(String input) {
+        List<String> day = new ArrayList<>();
+        day.add("일");
+        day.add("월");
+        day.add("화");
+        day.add("수");
+        day.add("목");
+        day.add("금");
+        day.add("토");
+
+        if (!day.contains(input)) {
+            throw new IllegalArgumentException(ERROR_MESSAGE);
+        }
+
+        return input;
     }
 }
--- src/main/java/oncall/domain/Month.java
+++ src/main/java/oncall/domain/Month.java
@@ -1,2 +1,21 @@
-package oncall.domain;public class Month {
+package oncall.domain;
+
+import oncall.OncallValidator;
+
+public class Month {
+    private final Integer month;
+    private final String startDay;
+
+    public Month(Integer month, String startDay) {
+        this.month = OncallValidator.checkMonth(month);
+        this.startDay = OncallValidator.checkStartDay(startDay);
+    }
+
+    public Integer getMonth() {
+        return month;
+    }
+
+    public String getStartDay() {
+        return startDay;
+    }
 }
