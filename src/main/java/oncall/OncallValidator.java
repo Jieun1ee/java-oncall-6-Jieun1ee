2495e8a847b2da92d0101ac2f85d1458dff7d7e1
LEE JIEUN <jieun3595@soongsil.ac.kr>
2023-12-16 14:45:20 +0900

    [FEAT] worker에 대한 Validation 작성
    .

--- src/main/java/oncall/OncallValidator.java
+++ src/main/java/oncall/OncallValidator.java
@@ -1,20 +1,15 @@
 package oncall;
 
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class OncallValidator {
     private static final String ERROR_MESSAGE = "[ERROR] 유효하지 않은 입력 값입니다. 다시 입력해 주세요.";
     private static final String COMMA = ",";
     private static final String SPACE = " ";
 
-    public static List<String> splitWithComma(String input) {
-        validateComma(input);
-        return Arrays.asList(input.split(COMMA));
-    }
-
-    private static void validateComma(String input) {
+    public static void validateComma(String input) {
         if (input.startsWith(COMMA) || input.endsWith(COMMA)) {
             throw new IllegalArgumentException(ERROR_MESSAGE);
         }
@@ -43,7 +38,63 @@ public class OncallValidator {
         if (!day.contains(input)) {
             throw new IllegalArgumentException(ERROR_MESSAGE);
         }
-
         return input;
+    }
+
+    public static Integer checkInteger(String num) {
+        try {
+            return Integer.parseInt(num);
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException(ERROR_MESSAGE);
+        }
+    }
+
+    public static void checkDuplicate(List<String> names) {
+        List<String> duplicates = names.stream()
+                .collect(Collectors.groupingBy(s -> s))
+                .entrySet()
+                .stream()
+                .filter(e -> e.getValue().size() > 1)
+                .map(e -> e.getKey())
+                .collect(Collectors.toList());
+
+        if (!duplicates.isEmpty()) {
+            throw new IllegalArgumentException(ERROR_MESSAGE);
+        }
+    }
+
+    public static void checkWorker(List<String> weekdayWorker, List<String> holidayWorker) {
+        checkPeopleNumber(weekdayWorker, holidayWorker);
+        checkName(weekdayWorker);
+        checkName(holidayWorker);
+        checkWorkers(weekdayWorker, holidayWorker);
+    }
+
+    private static void checkPeopleNumber(List<String> weekdayWorker, List<String> holidayWorker) {
+        if (weekdayWorker.size() != holidayWorker.size()) {
+            throw new IllegalArgumentException(ERROR_MESSAGE);
+        }
+        if (weekdayWorker.size() < 5 || weekdayWorker.size() > 35) {
+            throw new IllegalArgumentException(ERROR_MESSAGE);
+        }
+    }
+
+    private static void checkWorkers(List<String> weekdayWorker, List<String> holidayWorker) {
+        for (String name : weekdayWorker) {
+            if (!holidayWorker.contains(name)) {
+                throw new IllegalArgumentException(ERROR_MESSAGE);
+            }
+        }
+    }
+
+    private static void checkName(List<String> names) {
+        for (String name : names) {
+            if (name.length() > 5 || name.length() == 0) {
+                throw new IllegalArgumentException(ERROR_MESSAGE);
+            }
+        }
+        if (names.isEmpty()) {
+            throw new IllegalArgumentException(ERROR_MESSAGE);
+        }
     }
 }
