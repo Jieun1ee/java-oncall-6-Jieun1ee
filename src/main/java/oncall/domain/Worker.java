133f16658c5f4668691ed4ab5b973721788a5c49
LEE JIEUN <jieun3595@soongsil.ac.kr>
2023-12-16 14:52:30 +0900

    [FEAT] 평일 근무자와 휴일 근무자를 각각 가져올 수 있도록 기능 구현
    .

--- src/main/java/oncall/domain/Worker.java
+++ src/main/java/oncall/domain/Worker.java
@@ -4,8 +4,21 @@ import java.util.List;
 import oncall.OncallValidator;
 
 public class Worker {
+    private List<String> weekdayWorker;
+    private List<String> holidayWorker;
+
 
     public Worker(List<String> weekdayWorker, List<String> holidayWorker) {
         OncallValidator.checkWorker(weekdayWorker, holidayWorker);
+        this.weekdayWorker = weekdayWorker;
+        this.holidayWorker = holidayWorker;
+    }
+
+    public List<String> getWeekdayWorker() {
+        return weekdayWorker;
+    }
+
+    public List<String> getHolidayWorker() {
+        return holidayWorker;
     }
 }
